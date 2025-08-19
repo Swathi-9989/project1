251137eb2e7830061848f18941a67505e93dc98e
Swathi-9989 <saiswathi460@gmail.com>
2025-08-19 19:37:59 +0530

    water droplets

---
+++ 123.py
@@ -0,0 +1,25 @@
+import calendar
+year=int(input("enter year : "))
+print("main menu")
+num=int(input("enter u r choice :"))
+match num:
+    case 1:
+        a=year%2
+        if a==0:
+            print("even",a)
+        else:
+            print("odd")
+    case 2:
+        if is leap:
+            print("leap year")
+        else:
+            print("not")
+    case 3:
+        if year >0:
+            print("+ve")
+        else:
+            print("-ve")
+    case _:
+        print("exit")
+
+            
